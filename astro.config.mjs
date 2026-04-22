2a9d6c1068ad7ae39f3806de86e3945ffc50116d
Cuong Le <dal703631@csr-94608.utdallas.edu>
2026-04-21 21:55:38 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lechicuong2k3.github.io',
-  base: '/cuongchile.github.io/',
   output: 'static',
   integrations: [
     mdx(), 
